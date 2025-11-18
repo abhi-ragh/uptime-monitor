10fccc5a1962c4ebea70680e4f661793847b1338
Abhiragh A R <abhiragh0@gmail.com>
2025-11-18 10:07:46 +0530

    use httpx for an accurate healthcheck with response time calculation

--- routers/health.py
+++ routers/health.py
@@ -1,11 +1,24 @@
 from fastapi import APIRouter
-import requests
+import httpx,time
 
 router = APIRouter()
 
 @router.get("/healthcheck")
-def health(url : str):
-    response = requests.get(url)
-    return {
-        "status": response.status_code
+async def health(url : str):
+    start = time.perf_counter()
+    try:
+        async with httpx.AsyncClient(timeout=5.0) as client:
+            response = await client.get(url)
+        duration = (time.perf_counter() - start) * 1000
+        return {
+            "url": url,
+            "status": "up" if response.status_code < 500 else "down",
+            "status_code": response.status_code,
+            "response-time": f"{round(duration,2)} ms",
+        }
+    except Exception as e:
+        return{
+            "url":url,
+            "status":"down",
+            "error":e
         }
