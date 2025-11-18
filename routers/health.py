20603767363dc661c4786586cac8edcec06e5773
Abhiragh A R <abhiragh0@gmail.com>
2025-11-18 11:32:05 +0530

    Add More Accurate Error Checking

--- routers/health.py
+++ routers/health.py
@@ -15,10 +15,32 @@ async def health(url : str):
             "status": "up" if response.status_code < 500 else "down",
             "status_code": response.status_code,
             "response-time": f"{round(duration,2)} ms",
+            "redirects": len(response.history)    
+        }
+    except httpx.ConnectTimeout:
+        return {
+            "url": url,
+            "status": "Connect Timeout"
+        }
+    except httpx.ReadTimeout:
+        return{
+            "url":url,
+            "status":"Read Timeout"
+        }
+    except httpx.ConnectError:
+        return{
+            "url":url,
+            "status":"Connect Error"    
+            }    
+    except httpx.RequestError as e:
+        return{
+            "url":url,
+            "status":"Request Error",
+            "error":str(e)
         }
     except Exception as e:
         return{
             "url":url,
             "status":"down",
-            "error":e
+            "error":str(e)
         }
